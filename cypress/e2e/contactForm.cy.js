4c0c045fff9b81a003a3cabc966684bab8871a6e
Khoghulan <32198576+khoghu@users.noreply.github.com>
2024-03-03 14:06:11 +0800

    Update contactForm script

--- cypress/e2e/contactForm.cy.js
+++ cypress/e2e/contactForm.cy.js
@@ -7,28 +7,31 @@ describe('Contact form', () =>
 {
 	it('should be able to fill out the contact form', () =>
 	{
-		cy.visit('/')
+		cy.visit('/');
 
-			// Find and click on the link to navigate to the Contact page or form
-			cy.get('#menu-item-17180 > .fusion-bar-highlight').click();
+		// Find and click on the link to navigate to the Contact page or form
+		cy.get('a.fusion-bar-highlight span.menu-text').contains('Contact').click({ force: true })
 
-			// add an input value in array
-			const inputValues = ['Khoghu Test', 'khoghu@gmail.com', '01114567890', 'Aluminati', 'Tester', 'Testing Cypress'];
+		// add an input value in array
+		const inputValues = ['Khoghu Test', 'khoghu@gmail.com', '01114567890', 'Aluminati', 'Tester', 'Testing Cypress'];
 
-			// loop the element and add array input value
-			cy.get('div.contactUsForm input:not([type="submit"]), div.contactUsForm textarea')
-				.each(($element, index) => {
-					const inputValue = inputValues[index] || ''
-					cy.wrap($element).type(inputValue);
-				})
-				.then(() => {
-					// Click the submit button after filling in the inputs
-					// Comment the submit button because duplicate contact form record created
-					// cy.get('div.contactUsForm input[type="submit"]').click({ force: true });
-					// cy.wait(5000);
-
-					// Log a form submission
-					cy.log('Form submission successful');
-				});
+		// loop the element and add array input value
+		cy.get('div.contactUsForm')
+			.within(() => {
+				cy.get('input:not([type="submit"]), textarea.wpcf7-textarea')
+					.each(($element, index) => {
+						const inputValue = inputValues[index] || '';
+						cy.wrap($element).type(inputValue);
+					})
+					.then(() => {
+						// Click the submit button after filling in the inputs
+						// Comment the submit button because google captcha
+						// cy.get('input[type="submit"]').click();
+						// cy.wait(5000);
+	
+						// Log a form submission
+						cy.log('Form submission successful');
+					});
+			});
 	});
 });
